66ea15dedbb46c2a584a92a5ad675e9ad4638947
Mumatae <hudson.w.shi123@gmail.com>
2020-06-26 23:11:18 -0400

    Add travel command, finish Noot tutorial

--- cogs/game.py
+++ cogs/game.py
@@ -1,5 +1,7 @@
 import asyncio
+import glob
 import json
+import os
 import discord
 from discord.ext import commands
 from functions import pg, prodigy
@@ -24,14 +26,13 @@ class Game(commands.Cog):
                 return
 
             username = in_game['username']
-            query = 'SELECT is_new FROM players WHERE username = $1'
-            is_new = await pg.fetchrow(query, (in_game['username'],))
+            query = 'SELECT * FROM players WHERE username = $1'
+            row = await pg.fetchrow(query, (in_game['username'],))
 
-            if is_new:
+            if row['is_new']:
                 loop = asyncio.get_event_loop()
-                path = './resources/areas/A B.png'
+                image = Image.open('./resources/areas/A B.png')
 
-                image = Image.open(path)
                 buffer = await loop.run_in_executor(None, prodigy.noot, image, '4',
                                                     "Hi, and welcome to Pippet bot, the all original emulation of "
                                                     "Prodigy Math Game - in Discord!", 'Send a message in chat to '
@@ -63,8 +64,7 @@ class Game(commands.Cog):
                 await pg.execute(query, (gender, username))
 
                 text = "The name's Noot. Who might you be?"
-                buffer = await loop.run_in_executor(None, prodigy.noot, image, '3', text,
-                                                    "Enter a name in chat under 32 characters.")
+                buffer = prodigy.noot(image, '3', text, "Enter a name in chat under 32 characters.")
                 await ctx.send(file=discord.File(fp=buffer, filename='name.png'))
 
                 while True:
@@ -82,16 +82,15 @@ class Game(commands.Cog):
                 query = 'UPDATE players SET name = $1 WHERE username = $2'
                 await pg.execute(query, (name, username))
 
-                text = 'AMAZING NAME!!! omg TOTALLY NOT SARCASTIC!!! je moeder je moeder'
+                text = f'Nice to meet you, {name}.'
                 buffer = await loop.run_in_executor(None, prodigy.noot, image, '4', text,
                                                     "Send a message in chat to continue.")
                 await ctx.send(file=discord.File(fp=buffer, filename='name-completed.png'))
                 await self.bot.wait_for('message', check=check)
 
                 text = "Choose your starter pet! You don't have to battle it, since Stormtorch hasn't done that yet."
-                image = Image.open('./resources/areas/starters.png')
-                buffer = await loop.run_in_executor(None, prodigy.noot, image, '3', text,
-                                                    "Send either A, B, C, D, or E.")
+                buffer = await loop.run_in_executor(None, prodigy.noot, Image.open('./resources/areas/starters.png'),
+                                                    '3', text, "Send either A, B, C, D, or E.")
                 await ctx.send(file=discord.File(fp=buffer, filename='starter-pet.png'))
 
                 while True:
@@ -120,29 +119,71 @@ class Game(commands.Cog):
                 pets = json.loads(pets['pets'])
                 pets[pet] = 1
                 pets = json.dumps(pets)
-                query = 'UPDATE players SET pets = $1 WHERE username = $2'
-                await pg.execute(query, (pets, username))
+                query = 'UPDATE players SET pets = $1, is_new = $2 WHERE username = $3'
+                await pg.execute(query, (pets, False, username))
 
-                def new_starter():
-                    background = Image.open('./resources/areas/starters.png')
-                    background.putalpha(64)
-                    pet_image = Image.open(f'./resources/pets/{pet}.png')
-                    
-                    center_x, center_y = round(background.size[0] / 2), round(background.size[1] / 2)
-                    size_x, size_y = round(pet_image.size[0] / 2), round(pet_image.size[1] / 2)
-                    x, y = center_x - size_x, center_y - size_y
-                    
-                    background.paste(pet_image, (x, y))
-                    background_buffer = prodigy.noot(background, '4', 'You got your first pet!',
-                                                     f'Type "{ctx.prefix}travel B" in chat to continue to next area.')
+                background = await loop.run_in_executor(None, prodigy.new_item, 'common',
+                                                        './resources/areas/starters.png', pet)
+                buffer = await loop.run_in_executor(None, prodigy.noot, background, '4', 'You got your first pet!',
+                                                    f'Type "{ctx.prefix}travel B" in chat to continue to next area.')
 
-                    return background_buffer
-
-                buffer = await loop.run_in_executor(None, new_starter)
                 await ctx.send(file=discord.File(fp=buffer, filename='end.png'))
+            else:
+                location = row['location']
+                f = None
+
+                for file in os.listdir('./resources/areas'):
+                    if file.startswith(f'{location} '):
+                        f = f'./resources/areas/{file}'
+
+                if f:
+                    await ctx.send(file=discord.File(f))
 
         except Exception as error:
             await ctx.send(error)
+
+    @commands.command()
+    async def travel(self, ctx, location):
+        try:
+            yes, no = self.bot.gifs['yes'], self.bot.gifs['no']
+
+            query = 'SELECT username FROM credentials WHERE user_id = $1'
+            in_game = await pg.fetchrow(query, (ctx.author.id,))
+
+            if in_game is None:
+                await ctx.send(f'{no} You are not logged into an account.')
+                return
+
+            query = 'SELECT * FROM players WHERE username = $1'
+            row = await pg.fetchrow(query, (in_game['username'],))
+
+            if row['is_new']:
+                await ctx.send(f'{no} You need to complete the tutorial first with `{ctx.prefix}launch`.')
+                return
+
+            f = None
+            location_name = row['location']
+
+            for file in glob.glob(f'./resources/areas/{location_name} *'):
+                adjacent = file.replace('.png', '').split()[1:]
+
+                if location.upper() in adjacent:
+                    f = glob.glob(f'./resources/areas/{location.upper()} *.png')
+
+                    if f:
+                        f = f[0]
+                        break
+
+            query = 'UPDATE players SET location = $1 WHERE username = $2'
+            await pg.execute(query, (location.upper(), row['username']))
+
+            if f:
+                await ctx.send(file=discord.File(f))
+            else:
+                await ctx.send('Uh oh... you cannot reach that location now!')
+
+        except Exception as e:
+            await ctx.send(e)
 
 
 def setup(bot):
